7ec8ce0520734dfbb236c97a6ff2277da44815e7
Jennyfer <jennyfer.hubac@gmail.com>
2016-05-17 23:38:55 -0500

    CheckoutTransaction and checkin

---
+++ RentalFormApplication/entities/CheckoutTransaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RentalFramework.entities;
+
+namespace RentalFormApplication.entities
+{
+    public class CheckoutTransaction : ATransaction
+    {
+        public CheckoutTransaction(DateTime transactionDate, ARequest request) : base(transactionDate, request)
+        {
+
+        }
+        public override double getTotal()
+        {
+            return 0;
+        }
+
+        public override double getPenalty()
+        {
+            return 0;
+        }
+
+
+        public override string getType()
+        {
+            return "CHECKOUT";
+        }
+    }
+}
---
+++ RentalFormApplication/factories/TransactionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RentalFramework.factories;
+using RentalFramework.entities;
+using RentalFramework.dto;
+
+namespace RentalFormApplication.factories
+{
+    public class TransactionFactory : ITransactionFactory
+    {
+
+        public ATransaction create(ATransactionDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
